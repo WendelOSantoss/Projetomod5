64937282b25997242ee38779aab984d103ee001b
Wendel Santos <wendelosantos@hotmail.com>
2022-11-28 18:09:15 -0300

    feat: Criação do getuserbyid

--- aplicativocomida/src/user/services/user.services.ts
+++ aplicativocomida/src/user/services/user.services.ts
@@ -38,4 +38,15 @@ export class UserService {
         });
         return true
     }
+
+    async getUserById(userId: string): Promise<IUserEntity>{
+        const existUser = this.users.find((user) => user.id === userId);
+        if (!existUser) {
+            throw new Error ("User not found");
+        }
+        return existUser;
+
+    }
+
+
 }
