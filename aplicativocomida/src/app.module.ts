4608d9c039fda61ddfd5c0a17e6cd6afe25a516f
Wendel Santos <wendelosantos@hotmail.com>
2022-12-06 20:58:18 -0300

    feat: estrutura básica do crud profile

--- aplicativocomida/src/app.module.ts
+++ aplicativocomida/src/app.module.ts
@@ -3,10 +3,11 @@ import { DatabaseModule } from './prisma/database.module';
 import { UserService } from './user/services/user.services';
 import { UserController } from './user/user.controller';
 import { UserRepository } from './user/user.repository';
+import { ProfileModule } from './profile/profile.module';
 
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, ProfileModule],
     controllers: [UserController],
     providers: [UserService, UserRepository],
     
